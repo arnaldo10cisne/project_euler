47d3a3d2a5fdbe4b122b427f523b9a86a4bf67b4
arnaldomacheight <arnaldo10cisne@macheight.com>
2022-04-24 21:06:32 -0500

    Created basic structure. Added solutions for problemas 1 and 2

---
+++ solutions.py
@@ -0,0 +1,18 @@
+def p1():
+    sum = 0
+    for i in range(1000):
+        if (i % 3 == 0) or (i % 5 == 0):
+            sum += i
+    print(sum)
+
+
+def p2():
+    sum = 0
+    a, b = 1, 2
+    while b <= 4000000:
+        if (b % 2 == 0):
+            sum += b
+        aux = b
+        b = b + a
+        a = aux
+    print(sum)
